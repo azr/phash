ed268c04d3550b6028694db73c7f765390710a33
Adrien Delorme <adrien.delorme@icloud.com>
2017-12-29 16:07:27 +0100

    cmd: add WriteImageToPath helper func

--- cmd/utils.go
+++ cmd/utils.go
@@ -4,7 +4,7 @@ import (
 	"image"
 	// preload all image decoders
 	_ "image/gif"
-	_ "image/jpeg"
+	"image/jpeg"
 	_ "image/png"
 	"log"
 	"os"
@@ -16,6 +16,7 @@ import (
 
 // OpenImageFromPath load an image from full path
 // using image.Decode
+// or panics
 func OpenImageFromPath(path string) (img image.Image, format string) {
 	reader, err := os.Open(path)
 	if err != nil {
@@ -28,4 +29,18 @@ func OpenImageFromPath(path string) (img image.Image, format string) {
 		log.Fatal(err)
 	}
 	return m, s
+}
+
+// WriteImageToPath creates an image somewhere for you
+// or panics
+func WriteImageToPath(img image.Image, path string) {
+	f, err := os.Create(path + ".jpg")
+	if err != nil {
+		log.Fatalf("%s. Current dir: %s", err, os.Args[0])
+	}
+	defer f.Close()
+	err = jpeg.Encode(f, img, nil)
+	if err != nil {
+		log.Fatalf("Write err: %s. Current dir: %s", err, os.Args[0])
+	}
 }
